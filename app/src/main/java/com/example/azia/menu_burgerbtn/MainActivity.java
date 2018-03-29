8bb86f8d3ebe2094e6b4e1b054636c9e07114c82
Zezus <rustam-sadvakaso@mail.ru>
2018-03-29 18:03:13 +0600

    ready 0.5

--- app/src/main/java/com/example/azia/menu_burgerbtn/MainActivity.java
+++ app/src/main/java/com/example/azia/menu_burgerbtn/MainActivity.java
@@ -1,13 +1,15 @@
 package com.example.azia.menu_burgerbtn;
 
 import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.design.widget.NavigationView;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
-import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
 
     private DrawerLayout drawerLayout;
     private ActionBarDrawerToggle actionBarDrawerToggle;
@@ -22,21 +24,37 @@ public class MainActivity extends AppCompatActivity {
         drawerLayout.addDrawerListener(actionBarDrawerToggle);
         actionBarDrawerToggle.syncState();
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        NavigationView navigationView = findViewById(R.id.am_nav_view);
+        navigationView.setNavigationItemSelectedListener(this);
+
 
     }
 
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        return super.onCreateOptionsMenu(menu);
-    }
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
         if (actionBarDrawerToggle.onOptionsItemSelected(item)) {
+
             return true;
         }
+
+
         return super.onOptionsItemSelected(item);
     }
 
+
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.dm_about_name:
+                Toast.makeText(getApplicationContext(), item.getTitle(), Toast.LENGTH_SHORT).show();
+                drawerLayout.closeDrawers();
+                break;
+            case R.id.dm_settings_name:
+                Toast.makeText(getApplicationContext(), item.getTitle(), Toast.LENGTH_SHORT).show();
+                drawerLayout.closeDrawers();
+                break;
+        }
+        return false;
+    }
 }
