f1bc7d8c4f8f55b0b0258ed25e7b57cf8acdbe14
Zezus <rustam-sadvakaso@mail.ru>
2018-03-30 16:05:09 +0600

    end

--- app/src/main/java/com/example/azia/menu_burgerbtn/MainActivity.java
+++ app/src/main/java/com/example/azia/menu_burgerbtn/MainActivity.java
@@ -43,18 +43,44 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
     }
 
 
+
     @Override
     public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+        android.support.v4.app.Fragment fragment = null;
+        Class fragmentClass = null;
         switch (item.getItemId()) {
             case R.id.dm_about_name:
+                fragmentClass = AboutFragment.class;
                 Toast.makeText(getApplicationContext(), item.getTitle(), Toast.LENGTH_SHORT).show();
                 drawerLayout.closeDrawers();
                 break;
+
             case R.id.dm_settings_name:
+                fragmentClass = SettingsFragment.class;
                 Toast.makeText(getApplicationContext(), item.getTitle(), Toast.LENGTH_SHORT).show();
                 drawerLayout.closeDrawers();
                 break;
+
+            case R.id.dm_exit_name:
+                finish();
+                System.exit(0);
+                break;
+
+            default:
+                fragmentClass = AboutFragment.class;
         }
+
+        try {
+            fragment = (android.support.v4.app.Fragment) fragmentClass.newInstance();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        android.support.v4.app.FragmentManager fragmentManager = getSupportFragmentManager();
+        fragmentManager.beginTransaction().replace(R.id.am_context_ll, fragment).commit();
+        item.setChecked(true);
+        item.setTitle(item.getTitle());
+        drawerLayout.closeDrawers();
         return false;
     }
 }
